ea861dd4108311c928ab41108b388e0d0d0d411c
Auke Noppe <auke@noppe.nl>
2020-10-06 20:47:46 +0200

    convenience run script

---
+++ run-client.sh
@@ -0,0 +1,5 @@
+node dist/index.js \
+  --client_cert certs/device-aukes-device.crt \
+  --client_key certs/device-aukes-device.key \
+  --ca_cert certs/AmazonRootCA1.pem \
+  --endpoint <fill in your endpoint>
